36c012eeba80f695875dbd24633503224ddbfdc0
diogorodriguees <diogorodrigueslife@gmail.com>
2023-02-01 19:51:48 -0300

    layout base adicionado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,26 +7,28 @@ function App() {
 
   return (
     <div className="App">
+      <header>AUTO HK
+        <input type="text" placeholder='BUSCAR'/>
+      </header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h2>VEICULO</h2>
+          <button>+</button>
+        </div>
+
+        <div>
+          <div>
+            <h3>Lista de veiculos</h3>
+          </div>
+          <div>
+            <h3>Detalhes</h3>
+            <ul>
+              {/* cards com os carros */}
+            </ul>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </div>
   )
 }
