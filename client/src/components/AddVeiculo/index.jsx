839061c391472937471c375f76f401dd256c49b1
DiogoRodriguees <diogorodrigueslife@gmail.com>
2023-02-21 17:20:13 -0300

    :rocket: new style in button add

--- client/src/components/AddVeiculo/index.jsx
+++ client/src/components/AddVeiculo/index.jsx
@@ -9,20 +9,18 @@ export default function AddVeiculo(adicionarVeiculo) {
     const [novoVeiculo, setNovoVeiculo] = useState(false);
 
     function adicionarVeiculo() {
-        let status = novoVeiculo ? false : true
+        let status = novoVeiculo ? false : true;
         setNovoVeiculo(status);
     }
 
-    
     return (
         <div className="busca-container">
-
             <h2 className="busca-titulo">VEÍCULO</h2>
 
-            {/* <BsPlusCircleFill size="30px" className="add-veiculo" onClick={adicionarVeiculo}/> */}
-            <button className="add-veiculo">Adicionar</button>
-            {novoVeiculo && <Modal fecharModal={adicionarVeiculo}/>}
-
+            <button className="add-veiculo" onClick={adicionarVeiculo}>
+                Adicionar
+            </button>
+            {novoVeiculo && <Modal fecharModal={adicionarVeiculo} />}
         </div>
     );
 }
--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -10,7 +10,7 @@ export default function Modal({
     fecharModal,
     name = "",
     year = 0,
-    brand = "",
+    brand = "Chevrolet",
     description = "",
     sold = false,
     id = 0,
